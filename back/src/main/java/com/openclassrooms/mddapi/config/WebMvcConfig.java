a0ef8ce9900f60b176ff8026a425bee44684eee7
Ali0908 <zaabat97@gmail.com>
2024-09-28 11:04:28 +0200

    feat: display topics on front done.

---
+++ back/src/main/java/com/openclassrooms/mddapi/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.openclassrooms.mddapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
+                .allowedHeaders("Content-Type", "Origin", "Accept", "Authorization", "Content-Length", "X-Requested-With")
+                .allowCredentials(true);
+    }
+}
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,14 +5,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import {MatMenuModule} from "@angular/material/menu";
+import {MatIconModule} from "@angular/material/icon";
+import { TopicComponent } from './pages/topic/topic/topic.component';
+import {MatCardModule} from "@angular/material/card";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, TopicComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
