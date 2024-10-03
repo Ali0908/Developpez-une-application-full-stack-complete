46b1f5f9a73cae9f24cea435422b7a9fef394b69
Ali0908 <zaabat97@gmail.com>
2024-10-03 21:20:20 +0200

    add unsubcribe topic on frontend

---
+++ front/src/app/core/models/topic.ts
@@ -0,0 +1,7 @@
+export interface Topic {
+  description: string;
+  id: number;
+  name: string;
+  subscription: boolean;
+  userId: number;
+}
---
+++ front/src/app/core/models/unsubscribe-topic.ts
@@ -0,0 +1,4 @@
+export interface UnsubscribeTopic {
+  topicId: number;
+  userId: number;
+}
